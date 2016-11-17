efc5416d9399b1f19f561a7488b2eec3bc9131a0
Layer <gai00layer@gmail.com>
2016-11-17 08:13:20 +0000

    20161117: update export all

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 author: Layer <gai00layer@gmail.com>
 version: 1.0.1
 change logs:
+  1.0.2
+    export * 沒辦法處理 default，重新來一次 =x=
   1.0.1
     直接轉送store全部的東西
   1.0.0
     轉送store
 */
-export * from './store';
+import store, {Store} from './store';
+
+export default store;
+export {Store};
